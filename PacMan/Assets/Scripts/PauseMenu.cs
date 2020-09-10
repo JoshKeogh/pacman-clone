a66e4fbaf0a817f6a97d1c6c219d33e52aceb035
Josh Keogh <joshua.keogh@griffithuni.edu.au>
2020-09-10 18:36:51 +1000

    Pause menu
    The game can be paused by pressing the escape button, which brings up a menu overlay displaying the pause menu. The menu currently has a single button, Resume, which continues the game.

---
+++ PacMan/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        pauseMenuUI.SetActive(false);
+    }
+    
+    public void QuitGame()
+    {}
+}
